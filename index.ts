1463266586c49fbd0968ce38734bce376d4b7f84
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-11-07 11:22:41 +0100

    cors

--- index.ts
+++ index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { Storage } from "@google-cloud/storage";
 import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
+import morgan from "morgan";
 
 const PROJECT_NAME = process.env.GOOGLE_CLOUD_PROJECT || "excalidraw-json-dev";
 const PROD = PROJECT_NAME === "excalidraw-json";
-const DEV = !PROD;
+const LOCAL = !PROD;
 const BUCKET_NAME = PROD
   ? "excalidraw-json.appspot.com"
   : "excalidraw-json-dev.appspot.com";
@@ -19,44 +20,57 @@ const bucket = storage.bucket(BUCKET_NAME);
 const app = express();
 
 app.set("json spaces", 2);
+app.use(morgan("tiny"));
 
-const corsOptions = {
-  origin: DEV ? "*" : "excalidraw.com",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+const allowOrigins = [
+  "http://localhost:",
+  "https://www.excalidraw.com",
+  "https://excalidraw.com",
+  "excalidraw.vercel.app",
+  "https://dai-shi.github.io",
+];
+
+const corsGet = cors();
+const corsPost = cors((req, callback) => {
+  const origin = req.headers.origin;
+  let isGood = false;
+  if (origin) {
+    for (const allowOrigin of allowOrigins) {
+      if (origin.indexOf(allowOrigin) >= 0) {
+        isGood = true;
+        break;
+      }
+    }
+  }
+  callback(null, { origin: isGood });
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get("/.delme", (req, res) => {
-  res.json(process.env);
+  res.json({ headers: req.headers, env: process.env });
 });
 
-app.get("/api/v2/:key", (req, res) => {
-  const key = req.params.key;
-  // TODO: check what happens if the file does not exist
-  const stream = bucket.file(key).createReadStream();
-  let first = true;
-  stream.on("data", (data) => {
-    if (first) {
-      res.status(200);
-      res.setHeader("content-type", "text/plain"); // TODO: fix me to binary octet stream
-      first = false;
-    }
-    res.send(data);
-  });
-  stream.on("end", () => {
-    res.end();
-  });
-  stream.on("error", () => {
-    res.status(404);
-    res.end("404");
-  });
+app.get("/api/v2/:key", corsGet, async (req, res) => {
+  try {
+    const key = req.params.key;
+    const file = bucket.file(key);
+    const fileSize = (await file.getMetadata())[0].size;
+    res.status(200);
+    res.setHeader("content-type", "application/octet-stream");
+    res.setHeader("content-length", fileSize);
+    file.createReadStream().pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({
+      message: "Could not find the file.",
+    });
+  }
 });
 
-app.post("/api/v2/post/", (req, res) => {
+app.post("/api/v2/post/", corsGet, (req, res) => {
   const id = uuid();
   try {
     const blob = bucket.file(id);
@@ -64,14 +78,15 @@ app.post("/api/v2/post/", (req, res) => {
       resumable: false,
     });
 
-    blobStream.on("error", (err) => {
-      res.status(500).json({ message: err.message });
+    blobStream.on("error", (error) => {
+      console.error(error);
+      res.status(500).json({ message: error.message });
     });
 
     blobStream.on("finish", async () => {
       res.status(200).json({
         id,
-        data: `${DEV ? "http" : "https"}://${req.get("host")}/api/v2/${id}`,
+        data: `${LOCAL ? "http" : "https"}://${req.get("host")}/api/v2/${id}`,
       });
     });
 
@@ -84,13 +99,13 @@ app.post("/api/v2/post/", (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "Could not upload the data",
+      message: "Could not upload the data.",
     });
   }
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("http://localhost:" + port, "dev =", DEV));
+app.listen(port, () => console.log("http://localhost:" + port, "dev =", LOCAL));
 
 function uuid() {
   return uuidv4().replace(/-/g, "");
